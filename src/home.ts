86cbdd1752b9418e17f6ba700e86e9f1e4d12129
ecmel <ecmel.ercan@gmail.com>
2024-10-18 14:13:53 +0300

    release

--- src/home.ts
+++ src/home.ts
@@ -12,8 +12,14 @@ export class HomeView extends LitElement {
   static styles = [
     css`
       :host {
-        display: block;
-        padding: 2rem;
+        display: flex;
+        justify-content: center;
+      }
+
+      .container {
+        display: flex;
+        flex-flow: column nowrap;
+        justify-content: center;
       }
     `,
   ];
@@ -31,17 +37,45 @@ export class HomeView extends LitElement {
 
   protected render() {
     return html`
-      <p>
-        Welcome to my home page. My name is Ecmel Ercan. I am a software
-        developer. You can find my open source software on
-        <a href="https://github.com/ecmel">GtiHub</a>
-      </p>
-      <!-- 
-      <sl-button
-        href="itms-services://?action=download-manifest&url=https://ecmel.dev/defter/manifest.plist"
-        >Defter iOS</sl-button
-      >
-      -->
+      <div class="container">
+        <div>
+          <p>
+            Welcome to my home page. My name is Ecmel Ercan. I am a software
+            developer. You can find my open source work on
+            <a href="https://github.com/ecmel">GtiHub</a>
+          </p>
+          <p>
+            Also you can download beta versions of <strong>Defter</strong> for
+            <a
+              href="itms-services://?action=download-manifest&url=https://ecmel.dev/defter/manifest.plist"
+              >iOS</a
+            >
+            and
+            <a
+              href="https://github.com/ecmel/ecmel.dev/releases/download/2/app-universal-release.apk"
+              >Android</a
+            >
+            and <strong>Verditum</strong> for
+            <a
+              href="itms-services://?action=download-manifest&url=https://ecmel.dev/verditum/manifest.plist"
+              >iOS</a
+            >,
+            <a
+              href="https://github.com/ecmel/ecmel.dev/releases/download/1/app-universal-release.apk"
+              >Android</a
+            >,
+            <a
+              href="https://github.com/ecmel/ecmel.dev/releases/download/1/Verditum_0.0.1_aarch64.dmg"
+              >MacOS</a
+            >
+            and
+            <a
+              href="https://github.com/ecmel/ecmel.dev/releases/download/1/Verditum_0.0.1_x64-setup.exe"
+              >Windows</a
+            >.
+          </p>
+        </div>
+      </div>
     `;
   }
 }
